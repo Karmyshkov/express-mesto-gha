51e6d76a0b9f41a11730b292751168581fde3bdf
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-04-02 19:47:32 +0700

    feat: add email and password fileds for user model

--- models/User.js
+++ models/User.js
@@ -1,6 +1,23 @@
 const { Schema, model } = require('mongoose');
+import validator = require('validator');
 
 const schema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator(email) {
+        return validator.isEmail(email);
+      },
+      message: 'Некорректный email',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
   name: {
     type: String,
     required: true,
