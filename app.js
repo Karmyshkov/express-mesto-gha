75d794db233461046fb427ec3a0a1671c58d0d3f
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-04-04 17:32:28 +0700

    fix: change routing

--- app.js
+++ app.js
@@ -1,27 +1,21 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const { PORT, uri } = require('./config/config');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const { PORT, uri } = require("./config/config");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '62149e70772c6082b3373106',
-  };
-
-  next();
-});
-
-app.use('/', require('./routes/user'));
-app.use('/', require('./routes/card'));
-app.use(require('./middlewares/errorHandler'));
+app.post("/signin", require("./controllers/user").login);
+app.post("/signup", require("./controllers/user").createUser);
+app.use("/", require("./routes/user"));
+app.use("/", require("./routes/card"));
+app.use(require("./middlewares/errorHandler"));
 
 app.use((req, res) => {
-  res.status(404).send({ message: 'Страница не найдена' });
+  res.status(404).send({ message: "Страница не найдена" });
 });
 
 mongoose.connect(uri, {
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,9 @@
-const router = require('express').Router();
-const controller = require('../controllers/user');
+const router = require("express").Router();
+const controller = require("../controllers/user");
 
-router.get('/users', controller.getAllUsers);
-router.get('/users/:id', controller.getByIdUser);
-router.post('/users', controller.createUser);
-router.patch('/users/me', controller.editProfile);
-router.patch('/users/me/avatar', controller.editAvatar);
+router.get("/users", controller.getAllUsers);
+router.get("/users/:id", controller.getByIdUser);
+router.patch("/users/me", controller.editProfile);
+router.patch("/users/me/avatar", controller.editAvatar);
 
 module.exports = router;
