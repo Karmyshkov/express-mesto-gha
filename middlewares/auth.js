11094b4c56aa66b25258e201cd2400bba5c2bf9a
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-04-06 23:24:11 +0700

    refactoring auth middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,15 +4,10 @@ const UnauthorizedError = require('../errors/UnauthorizedError');
 const { SECRET_KEY = 'SECRET_KEY_TEST' } = process.env;
 
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new UnauthorizedError('Необходима авторизация');
-  }
-  const token = authorization.replace('Bearer ', '');
+  const token = req.cookies.jwt;
   let payload;
-
   try {
-    payload = jwt.verify(SECRET_KEY);
+    payload = jwt.verify(token, SECRET_KEY);
   } catch (err) {
     throw new UnauthorizedError('Необходима авторизация');
   }
