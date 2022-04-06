67bb5d4f281d94b4f352655e55bf96c74f9e6ff2
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-04-06 20:33:53 +0700

    refactoring user route

--- routes/user.js
+++ routes/user.js
@@ -1,19 +1,21 @@
 const router = require("express").Router();
-const controller = require("../controllers/user");
-const middleware = require("../middlewares/validation");
+const {
+  getAllUsers,
+  getByIdUser,
+  getCurrentUser,
+  editProfile,
+  editAvatar,
+} = require("../controllers/user");
+const {
+  IdValidate,
+  editProfileValidate,
+  editAvatarValidate,
+} = require("../middlewares/validation");
 
-router.get("/users", controller.getAllUsers);
-router.get("/users/:id", middleware.IdValidate, controller.getByIdUser);
-router.get("/users/me", controller.getCurrentUser);
-router.patch(
-  "/users/me",
-  middleware.editProfileValidate,
-  controller.editProfile
-);
-router.patch(
-  "/users/me/avatar",
-  middleware.editAvatarValidate,
-  controller.editAvatar
-);
+router.get("/users", getAllUsers);
+router.get("/users/:id", IdValidate, getByIdUser);
+router.get("/users/me", getCurrentUser);
+router.patch("/users/me", IdValidate, editProfileValidate, editProfile);
+router.patch("/users/me/avatar", IdValidate, editAvatarValidate, editAvatar);
 
 module.exports = router;
