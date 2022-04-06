1a4805788382579db78ac6d23318252dacdb3689
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-04-06 16:54:41 +0700

    added middlewares for card routes

--- routes/card.js
+++ routes/card.js
@@ -1,10 +1,15 @@
-const router = require('express').Router();
-const controller = require('../controllers/card');
+const router = require("express").Router();
+const controller = require("../controllers/card");
+const middleware = require("../middlewares/validation");
 
-router.get('/cards', controller.getAllCards);
-router.post('/cards', controller.createCard);
-router.delete('/cards/:id', controller.deleteByIdCard);
-router.put('/cards/:cardId/likes', controller.likeCard);
-router.delete('/cards/:cardId/likes', controller.dislikeCard);
+router.get("/cards", controller.getAllCards);
+router.post("/cards", middleware.createCard, controller.createCard);
+router.delete("/cards/:id", middleware.IdValidate, controller.deleteByIdCard);
+router.put("/cards/:cardId/likes", middleware.IdValidate, controller.likeCard);
+router.delete(
+  "/cards/:cardId/likes",
+  middleware.IdValidate,
+  controller.dislikeCard
+);
 
 module.exports = router;
