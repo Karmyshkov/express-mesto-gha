5bae286f7b1f798b1080ee977819bb6b52c94cab
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-04-06 23:23:41 +0700

    added cookeie-parser

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const NotFoundError = require('./errors/NotFoundError');
 const auth = require('./middlewares/auth');
 const {
@@ -16,6 +17,8 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use(cookieParser());
 
 app.post('/signin', loginValidate, login);
 app.post('/signup', createUserValidate, createUser);
