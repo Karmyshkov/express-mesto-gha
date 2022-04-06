40d181a28d43e8fcef7a53c8571bd4efc67a4be7
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-04-06 17:06:36 +0700

    added middlewares for user routes

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,19 @@
-const router = require('express').Router();
-const controller = require('../controllers/user');
+const router = require("express").Router();
+const controller = require("../controllers/user");
+const middleware = require("../middlewares/validation");
 
-router.get('/users', controller.getAllUsers);
-router.get('/users/:id', controller.getByIdUser);
-router.get('/users/me', controller.getCurrentUser);
-router.patch('/users/me', controller.editProfile);
-router.patch('/users/me/avatar', controller.editAvatar);
+router.get("/users", controller.getAllUsers);
+router.get("/users/:id", middleware.IdValidate, controller.getByIdUser);
+router.get("/users/me", controller.getCurrentUser);
+router.patch(
+  "/users/me",
+  middleware.editProfileValidate,
+  controller.editProfile
+);
+router.patch(
+  "/users/me/avatar",
+  middleware.editAvatarValidate,
+  controller.editAvatar
+);
 
 module.exports = router;
